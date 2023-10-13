8d9863fcd0fef29afd7a1d17ec995865c752793b
tahawy111 <amertahawy111@gmail.com>
2023-10-14 01:51:33 +0300

    .

--- dist/index.js
+++ dist/index.js
@@ -4,17 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const puppeteer_1 = __importDefault(require("puppeteer"));
+const url = "https://www.amazon.com/s?k=amazon+basics&page=7";
 (async () => {
-    const browser = await puppeteer_1.default.launch({
-        defaultViewport: null,
-        userDataDir: "./tmp",
-    });
+    const browser = await puppeteer_1.default.launch();
     const page = await browser.newPage();
-    await page.goto("https://www.amazon.eg/s?k=iphone&crid=17O42F9BHNEZI&sprefix=iphone%2Caps%2C279&ref=nb_sb_noss_1");
-    const productHandles = await page.$$("div.a-section.a-spacing-base");
-    for (const card of productHandles) {
-        console.log(await card.$(".a-size-base-plus"));
+    await page.goto(url);
+    const productsHandles = await page.$$(".puis-card-container > .a-section.a-spacing-base");
+    let totalProducts = [];
+    for (const productHandle of productsHandles) {
+        const product = {
+            title: await page.evaluate((document) => document.querySelector(".a-section h2 > a > span")?.textContent, productHandle),
+            price: await page.evaluate((document) => document.querySelector(".a-price > .a-offscreen")?.textContent, productHandle),
+            image: await page.evaluate((document) => document.querySelector(".s-image")?.getAttribute("src"), productHandle),
+        };
+        totalProducts.push(product);
     }
+    console.log(totalProducts);
+    console.log(totalProducts.length);
     await browser.close();
 })();
 //# sourceMappingURL=index.js.map
