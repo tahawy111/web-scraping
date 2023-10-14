b61d1fe7265483dadffedcf7cb74b2bae6cc0f55
tahawy111 <amertahawy111@gmail.com>
2023-10-14 23:40:21 +0300

    another model

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 // Import puppeteer
 import puppeteer, { Browser } from "puppeteer";
-import fs from "fs";
 
 const url = "https://www.amazon.com/s?k=amazon+basics&page=1";
 
@@ -16,44 +15,52 @@ const url = "https://www.amazon.com/s?k=amazon+basics&page=1";
 
   let isBtnDisabled = false;
 
-  fs.writeFile("results.csv", `title,price,image
-  `, () => {});
-
   while (!isBtnDisabled) {
-    const totalProducts: any[] = [];
+    // const totalProducts: any[] = [];
     await page.waitForSelector(`.puis-card-container`);
-    const productsHandles = await page.$$(
-      ".puis-card-container > .a-section.a-spacing-base"
-    );
-    for (const productHandle of productsHandles) {
-      const product = {
-        title: await page.evaluate(
-          (document) =>
-            document.querySelector(".a-section h2 > a > span")?.textContent,
-          productHandle
-        ),
-        price: await page.evaluate(
-          (document) =>
-            document.querySelector(".a-price > .a-offscreen")?.textContent,
-          productHandle
-        ),
-        image: await page.evaluate(
-          (document) => document.querySelector(".s-image")?.getAttribute("src"),
-          productHandle
-        ),
-      };
+    // const productsHandles = await page.$$(
+    //   ".puis-card-container > .a-section.a-spacing-base"
+    // );
+    // for (const productHandle of productsHandles) {
+    //   const product = {
+    //     title: await page.evaluate(
+    //       (document) =>
+    //         document.querySelector(".a-section h2 > a > span")?.textContent,
+    //       productHandle
+    //     ),
+    //     price: await page.evaluate(
+    //       (document) =>
+    //         document.querySelector(".a-price > .a-offscreen")?.textContent,
+    //       productHandle
+    //     ),
+    //     image: await page.evaluate(
+    //       (document) => document.querySelector(".s-image")?.getAttribute("src"),
+    //       productHandle
+    //     ),
+    //   };
 
-      totalProducts.push(product);
-      fs.appendFile(
-        "results.csv",
-        `${product.title?.replace(/,/g,".")}, ${product.price}, ${product.image}
-        `,
-        (err) => {
-          if(err) throw err
-          console.log("Saved!")
-        }
-      );
-    }
+    //   totalProducts.push(product);
+    // }
+
+    const items = await page.evaluate(() => {
+      const products = Array.from(
+        document.querySelectorAll(
+          ".puis-card-container > .a-section.a-spacing-base"
+        )
+      ).map((product) => {
+        const p = {
+          title: product.querySelector(".a-section h2 > a > span")?.textContent,
+          price: product.querySelector(".a-price > .a-offscreen")?.textContent,
+          image: product.querySelector(".s-image")?.getAttribute("src"),
+        };
+        return p;
+      });
+
+      return products
+    });
+
+    console.log(items);
+    
 
     await page.waitForSelector(".s-pagination-item.s-pagination-next", {
       visible: true,
